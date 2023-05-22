369154e2960449a27fd25073f36ba89264cc9208
Mohamed Amine LEGHERABA <mlegheraba@protonmail.com>
2023-05-22 03:24:22 +0200

    new tests

--- test/WrappedENaira.t.sol
+++ test/WrappedENaira.t.sol
@@ -4,16 +4,15 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/WrappedENaira.sol";
 
-// todo:
-// check why msg.sender amount is 0
-// msg.sender amount should equals initial amount
-// check only owner should be able to mint
-
 contract WrappedTest is Test {
     WrappedENaira public wrapped;
+    address public deployer;
+    uint256 public initialAmount;
 
     function setUp() public {
         wrapped = new WrappedENaira();
+        deployer = 0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496;
+        initialAmount = 1000 * 10 ** wrapped.decimals();
     }
 
     function testName() public {
@@ -24,25 +23,38 @@ contract WrappedTest is Test {
         assertEq(wrapped.symbol(), "WENGN");
     }
 
+    function testDecimals() public {
+        assertEq(wrapped.decimals(), 18);
+    }
+
     function testInitialSupply() public {
-        assertEq(wrapped.totalSupply(), 1000 * 10 ** wrapped.decimals());
+        assertEq(wrapped.totalSupply(), initialAmount);
     }
 
     function testBalanceMinter() public {
-        console2.log("amount is: ");
-        console2.log(wrapped.balanceOf(msg.sender));
-        assertEq(wrapped.totalSupply(), wrapped.balanceOf(msg.sender));
+        assertEq(initialAmount, wrapped.balanceOf(deployer));
+        assertEq(wrapped.totalSupply(), wrapped.balanceOf(deployer));
     }
 
     function testMint() public {
         uint256 value = 100 * 10 ** wrapped.decimals();
-        wrapped.mint(msg.sender, value);
-        assertEq(wrapped.totalSupply(), 1100 * 10 ** wrapped.decimals());
+        uint256 newValue = initialAmount + value;
+        wrapped.mint(deployer, value);
+        assertEq(wrapped.totalSupply(), newValue);
+        assertEq(wrapped.balanceOf(deployer), newValue);
     }
-
+    
+    function test_When_MinterIsNotOwner() public {
+        uint256 value = 100 * 10 ** wrapped.decimals();
+        vm.expectRevert("Ownable: caller is not the owner");
+        vm.prank(address(0));
+        wrapped.mint(deployer, value);
+    }
+    
     function testBurn() public {
         uint256 value = 100 * 10 ** wrapped.decimals();
+        uint256 newValue = initialAmount - value;
         wrapped.burn(value);
-        assertEq(wrapped.totalSupply(), 900 * 10 ** wrapped.decimals());
+        assertEq(wrapped.totalSupply(), newValue);
     }
 }
